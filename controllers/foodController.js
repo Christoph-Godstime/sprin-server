b1b255c6e122341c05706cf97c6bc5b9ba350ed8
Christopher Godstime <christophergodstime45@gmail.com>
2024-12-10 10:05:24 +0100

    search index

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -416,31 +416,12 @@ module.exports = {
         {
           $search: {
             index: "foods",
-            compound: {
-              should: [
-                {
-                  text: {
-                    query: search,
-                    path: ["title", "restaurantName", "description"], // Fields to search
-                    fuzzy: {
-                      maxEdits: 1, // Allows for typo tolerance
-                    },
-                  },
-                },
-                {
-                  autocomplete: {
-                    query: search,
-                    path: ["title", "restaurantName", "description"], // Fields to autocomplete
-                    tokenOrder: "sequential",
-                  },
-                },
-                {
-                  phrase: {
-                    query: search,
-                    path: ["title", "restaurantName", "description"], // Fields to search phrases
-                  },
-                },
-              ],
+            text: {
+              query: search,
+              path: ["title", "restaurantName", "description"], // Fields to search
+              fuzzy: {
+                maxEdits: 1, // Allows for typo tolerance
+              },
             },
           },
         },
@@ -454,7 +435,7 @@ module.exports = {
           $sort: { random: 1 }, // Sort by the random number to shuffle
         },
         {
-          $limit: 50, // Limit to 50 documents
+          $limit: 50, // Limit to 30 documents
         },
         {
           $project: { random: 0 }, // Remove the random field from the output
