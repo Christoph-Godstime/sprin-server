e4fe454b9f99c9b931c3507cb2931b917244887e
Christopher Godstime <christophergodstime45@gmail.com>
2025-07-27 15:17:32 +0100

    cold scoop free delivery

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -161,7 +161,7 @@ module.exports = {
       const isFreeDeliveryDay =
         today.getFullYear() === 2025 &&
         today.getMonth() === 6 && // July is 6 (zero-indexed)
-        today.getDate() === 24;
+        today.getDate() === 27;
 
       const orderCount = await Order.countDocuments({
         userId: new mongoose.Types.ObjectId(userId),
